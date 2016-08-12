b906be1563fdc13b1ed81adaaa58ba9e94d55a22
Angel Hernandez <ah.sec7@gmail.com>
2016-08-11 17:40:02 -0700

    exercise_02.py

---
+++ class2/exercise_02.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+"""
+script that connects using telnet to the pynet-rtr1 router. Execute the 
+'show ip int brief' command on the router and return the output
+"""
+
+import time
+import exercise_03
+
+def main():
+    username = 'pyclass'
+    password = '88newclass'
+    ip_address = '184.105.247.70'
+
+    dev = exercise_03.NetDevice(ip_address, username, password)
+
+    time.sleep(1)
+    output = dev.login()
+    print output
+
+    time.sleep(1)
+    dev.send_command("sh ip int brief")
+
+    dev.close()
+
+if __name__ == '__main__':
+    main()
+
