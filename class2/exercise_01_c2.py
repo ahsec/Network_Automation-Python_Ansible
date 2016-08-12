7484dc06a55a24cee02ef47038c6690cda630a69
Angel Hernandez <ah.sec7@gmail.com>
2016-08-11 17:39:35 -0700

    Adding exercise_01_c2.py

---
+++ class2/exercise_01_c2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+"""
+Script that imports a function from python script exercise_01_c1.py
+"""
+import exercise_01_c1
+
+def main():
+    exercise_01_c1.print_hello()
+
+if __name__ == '__main__':
+    main()
+
