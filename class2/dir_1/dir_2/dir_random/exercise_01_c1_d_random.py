b3db253cbf4fa2a5ef5adb19e80e50b84aa6c005
Angel Hernandez <ah.sec7@gmail.com>
2016-08-11 17:40:48 -0700

    Adding ./dir_1/dir_2/dir_random/exercise_01_c1_d_random.py

---
+++ class2/dir_1/dir_2/dir_random/exercise_01_c1_d_random.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+"""
+Simple python function that prints Hello
+"""
+
+def print_hello():
+    print "Hello"
+
+def main():
+    print_hello()
+
+if __name__ == '__main__':
+    main()
+
