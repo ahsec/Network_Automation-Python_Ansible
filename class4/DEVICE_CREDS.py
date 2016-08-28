b4131932de0c2d1b24a8bcd3f859ec6b0d435a05
Angel Hernandez <ah.sec7@gmail.com>
2016-08-28 08:08:01 -0700

    Adding $entry

---
+++ class4/DEVICE_CREDS.py
@@ -0,0 +1,33 @@
+cisco_881_1 = {
+    'device_type': 'cisco_ios',
+    'ip':   '184.105.247.70',
+    'username': 'pyclass',
+    'password': '88newclass',
+    'secret': 'secret',
+    'verbose': False,
+}
+
+cisco_881_2 = {
+    'device_type': 'cisco_ios',
+    'ip':   '184.105.247.71',
+    'username': 'pyclass',
+    'password': '88newclass',
+    'secret': 'secret',
+    'verbose': False,
+}
+
+juniper_srx = {
+    'device_type': 'juniper',
+    'ip': '184.105.247.76',
+    'username' : 'pyclass',
+    'password': '88newclass',
+    'secret': 'secret',
+    'verbose': False,
+}
+
+
+all_devices = [
+    cisco_881_1,
+    cisco_881_2,
+    juniper_srx,
+]
