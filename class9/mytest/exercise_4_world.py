9dc784ca293dafc64bdb4304a146971187273744
Angel Hernandez <ah.sec7@gmail.com>
2016-10-02 19:58:03 -0700

    Adding file

---
+++ class9/mytest/exercise_4_world.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+def func3():
+    print "From world.py"
+
+class MyClass(object):
+    def __init__(self, var_a, var_b, var_c):
+        self.var_a = var_a
+        self.var_b = var_b
+        self.var_c = var_c
+    def hello(self):
+        print 'Method - Hello from MyClass. Vars are: {} {} {}'.format(self.var_a,
+                                                                       self.var_b, self.var_c)
+    def not_hello(self):
+        print 'Method - Not Hello from MyClass. Vars are: {} {} {}'.format(self.var_a,
+                                                                           self.var_b, self.var_c)
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
