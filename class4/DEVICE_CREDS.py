977d0e63487c17f300e986071dfc9ce5875a8910
ahsec <ah.sec7@gmail.com>
2017-06-20 22:53:14 -0400

    Update DEVICE_CREDS.py

--- class4/DEVICE_CREDS.py
+++ class4/DEVICE_CREDS.py
@@ -1,27 +1,27 @@
 cisco_881_1 = {
     'device_type': 'cisco_ios',
-    'ip':   '184.105.247.70',
-    'username': 'pyclass',
-    'password': '88newclass',
-    'secret': 'secret',
+    'ip':   'x.x.x.x',
+    'username': 'USERNAME',
+    'password': 'PASSWD',
+    'secret': 'SECRET',
     'verbose': False,
 }
 
 cisco_881_2 = {
     'device_type': 'cisco_ios',
-    'ip':   '184.105.247.71',
-    'username': 'pyclass',
-    'password': '88newclass',
-    'secret': 'secret',
+    'ip':   'x.x.x.x',
+    'username': 'USERNAME',
+    'password': 'PASSWD',
+    'secret': 'SECRET',
     'verbose': False,
 }
 
 juniper_srx = {
     'device_type': 'juniper',
-    'ip': '184.105.247.76',
-    'username' : 'pyclass',
-    'password': '88newclass',
-    'secret': 'secret',
+    'ip': 'x.x.x.x',
+    'username' : 'USERNAME',
+    'password': 'PASSWD',
+    'secret': 'SECRET',
     'verbose': False,
 }
 
